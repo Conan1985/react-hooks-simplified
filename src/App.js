213db9af8b73018d96a1cdb46b38bc5e8ce14414
Conan1985 <Conan1985@users.noreply.github.com>
2024-01-21 08:11:13 -0600

    Customize useContext

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import {useEffect, useState} from "react";
+import React from "react";
+import FunctionContextComponent from "./FunctionContextComponent";
+import {ThemeProvider} from "./ThemeContext";
 
 function App() {
-    // const [resourceType, setResourceType] = useState('posts')
-    // const [items, setItmes] = useState()
-    //
-    // useEffect(() => {
-    //     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-    //         .then(response => response.json())
-    //         .then(json => setItmes(json))
-    // },[resourceType])
-
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-    const handleResize = () => {
-        setWindowWidth(window.innerWidth)
-    }
-
-    useEffect(() => {
-        window.addEventListener("resize", handleResize)
-
-        return () => {
-            window.removeEventListener("resize", handleResize)
-        }
-    }, []);
-
     return (
-        // <>
-        //     <div>
-        //         <button onClick={() => setResourceType('posts')}>Posts</button>
-        //         <button onClick={() => setResourceType('users')}>Users</button>
-        //         <button onClick={() => setResourceType('comments')}>Comments</button>
-        //     </div>
-        //     <h1>{resourceType}</h1>
-        //     {items.map(item => (
-        //         <pre>{JSON.stringify(item)}</pre>
-        //     ))}
-        // </>
-
-        <div>{windowWidth}</div>
-    );
+        <ThemeProvider>
+            <FunctionContextComponent/>
+        </ThemeProvider>
+    )
 }
 
-export default App;
+export default App
