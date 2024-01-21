9d8d6aed5168d30c6f30ad9de4e6ac8d1ed37942
Conan1985 <Conan1985@users.noreply.github.com>
2024-01-21 08:47:10 -0600

    UseRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
-import FunctionContextComponent from "./FunctionContextComponent";
-import {ThemeProvider} from "./ThemeContext";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
+    const [name, setName] = useState('')
+    // const renderCount = useRef(0)
+    const prevName = useRef('')
+    const inputRef = useRef()
+
+    // useEffect(() => {
+    //     renderCount.current = renderCount.current + 1
+    // });
+
+    function focus() {
+        inputRef.current.focus()
+        inputRef.current.value = 'focus button'
+    }
+
+    useEffect(() => {
+        prevName.current = name
+    }, [name]);
+
     return (
-        <ThemeProvider>
-            <FunctionContextComponent/>
-        </ThemeProvider>
+        <>
+            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
+            <div>My name is {name} and it used to be {prevName.current}</div>
+            {/*<div>I rendered {renderedderCount.current} time</div>*/}
+            <button onClick={focus}>Focus</button>
+        </>
     )
 }
 
