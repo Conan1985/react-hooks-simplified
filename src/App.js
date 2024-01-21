befd07d2ae3bb8386819bb07d18d4314428f1e5e
Conan1985 <Conan1985@users.noreply.github.com>
2024-01-21 09:10:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import {useEffect, useRef, useState} from "react";
+import {useMemo, useState} from "react";
 
 function App() {
-    const [name, setName] = useState('')
-    // const renderCount = useRef(0)
-    const prevName = useRef('')
-    const inputRef = useRef()
-
-    // useEffect(() => {
-    //     renderCount.current = renderCount.current + 1
-    // });
-
-    function focus() {
-        inputRef.current.focus()
-        inputRef.current.value = 'focus button'
+    const [number, setNumber] = useState(0)
+    const [dark, setDark] = useState(false)
+    const doubleNumber = useMemo(() => {return slowFunction(number)}, [number])
+    const themeStyles = {
+        backgroundColor: dark ? 'black' : 'white',
+        color: dark ? 'white' : 'black'
     }
-
-    useEffect(() => {
-        prevName.current = name
-    }, [name]);
 
     return (
         <>
-            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
-            <div>My name is {name} and it used to be {prevName.current}</div>
-            {/*<div>I rendered {renderedderCount.current} time</div>*/}
-            <button onClick={focus}>Focus</button>
+            <input type={number} value={number} onChange={e => setNumber(parseInt(e.target.value))}/>
+            <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+            <div style={themeStyles}>{doubleNumber}</div>
         </>
     )
+
+    function slowFunction(num) {
+        console.log('Calling slow function')
+        for (let i = 0; i < 1000000000; i++) {}
+            return num * 2
+
+    }
+
 }
 
 export default App
