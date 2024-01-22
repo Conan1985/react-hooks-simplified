2259804ed86418cff2b7dd36e3faa8d7e678d018
Conan1985 <Conan1985@users.noreply.github.com>
2024-01-22 09:56:29 -0600

    Update useTransition

--- src/App.js
+++ src/App.js
@@ -1,84 +1,28 @@
-import {useReducer, useState} from "react";
-import Todo from "./Todo";
-
-// const ACTIONS = {
-//     INCREMENT: 'increment',
-//     DECREMENT: 'decrement'
-// }
-
-export const ACTIONS = {
-    ADD_TODO: 'add-todo',
-    TOGGLE_TODO: 'toggle-todo',
-    DELETE_TODO: 'delete-todo'
-}
-
-// function reducer(state, action) {
-//     switch(action.type) {
-//         case ACTIONS.INCREMENT:
-//             return {count: state.count + 1}
-//         case ACTIONS.DECREMENT:
-//             return {count: state.count - 1}
-//         default:
-//             return state
-//     }
-// }
-
-function reducer(todos, action) {
-    switch(action.type) {
-        case ACTIONS.ADD_TODO:
-            return [...todos, newTodo(action.payload.name)]
-        case ACTIONS.TOGGLE_TODO:
-            return todos.map(todo => {
-                if (todo.id === action.payload.id) {
-                    return {...todo, complete: !todo.complete}
-                }
-                return todo
-            })
-        case ACTIONS.DELETE_TODO:
-            return todos.filter(todo => todo.id !== action.payload.id)
-        default:
-            return todos
-    }
-}
-
-function newTodo(name) {
-    return {id: Date.now(), name: name, complete: false}
-}
+import {useState, useTransition} from "react";
 
 function App() {
-    // const [state, dispatch] = useReducer(reducer, {count: 0})
-    const [todos, dispatch] = useReducer(reducer, [])
-    const [name, setName] = useState('')
+    const [isPending, startTransition] = useTransition()
+    const [input, setInput] = useState("")
+    const [list, setList] = useState([])
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        dispatch({type: ACTIONS.ADD_TODO, payload: {name: name}})
-        setName('')
+    const LIST_SIZE = 20000
+
+    function handlechange(e) {
+        setInput(e.target.value)
+        startTransition(() => {
+            const l = []
+            for (let i = 0; i < LIST_SIZE; i++) {
+                l.push(e.target.value)
+            }
+            setList(l)
+        })
     }
-
-    // function increment() {
-    //     dispatch({type: ACTIONS.INCREMENT})
-    // }
-    //
-    // function decrement() {
-    //     dispatch({type: ACTIONS.DECREMENT})
-    // }
-
-    // return (
-    //     <>
-    //         <button onClick={decrement}>-</button>
-    //         <span>{state.count}</span>
-    //         <button onClick={increment}>+</button>
-    //     </>
-    // )
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input type={"text"} value={name} onChange={e => setName(e.target.value)}/>
-            </form>
-            {todos.map(todo => {
-                return <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+            <input type={"text"} value={input} onChange={handlechange}/>
+            {isPending ? "Loading..." : list.map((item, index) => {
+                return <div key={index}>{item}</div>
             })}
         </>
     )
